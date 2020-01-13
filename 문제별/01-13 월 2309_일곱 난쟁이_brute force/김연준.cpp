eeb7dbef1971ec047d356d225cc8bcf03e3bae4c
KimYeonJun <duswns1783@naver.com>
2020-01-13 21:42:58 +0900

    Create 김연준.cpp
    ㅁㄴㅇ

---
+++ 문제별/01-13 월 2309_일곱 난쟁이_brute force/김연준.cpp
@@ -0,0 +1,43 @@
+﻿/*
+BOJ 2309 일곱 난쟁이
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> height_list;
+int height_sum = 0;
+void input() {
+	for (int i = 0; i < 9; i++) {
+		int height;
+		cin >> height;
+		height_list.push_back(height);
+		height_sum += height;
+	}
+}
+void solve() {
+	for (int i = 0; i < height_list.size() - 1; i++) {
+		for (int j = i + 1; j < height_list.size(); j++) {
+			if (height_sum - (height_list[i] + height_list[j]) == 100) {
+				height_list[i] = -1;
+				height_list[j] = -1;
+				return;
+			}
+		}
+	}
+	return;
+}
+void print() {
+	sort(height_list.begin(), height_list.end());
+	for (int i = 2; i < height_list.size(); i++) {
+		cout << height_list[i] << endl;
+	}
+}
+int main() {
+	input();
+	solve();
+	print();
+
+
+	return 0;
+}
