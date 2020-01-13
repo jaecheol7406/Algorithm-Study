c2f4aeed80ac589f0a6d238cf3efe13e8ebf1f92
KimYeonJun <duswns1783@naver.com>
2020-01-13 20:47:10 +0900

    Create 김연준.cpp

---
+++ 문제별/01-13 월 1065_한수_brute force/김연준.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void sol1(int N) {
+	int result = 0;
+	for (int i = 1; i <= N; i++) {
+		if (1 <= i && i <= 99) { //한자리, 두자리인 경우
+			result++;
+		}
+		else if (i == 1000) {
+			break;
+		}
+		else {
+			vector<int> list;
+			int temp = i;
+			while (temp != 0) {
+				list.push_back(temp % 10);
+				temp = temp / 10;
+			}
+			int sub1 = list[0] - list[1];
+			int sub2 = list[1] - list[2];
+			if (sub1 == sub2)
+				result++;
+		}
+
+	}
+	cout << result << endl;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	sol1(N);
+
+	return 0;
+}
