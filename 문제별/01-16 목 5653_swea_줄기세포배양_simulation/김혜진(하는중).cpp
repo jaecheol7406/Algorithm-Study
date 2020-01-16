23fd37cfb6d2bd5a928a7a0f813435e1ed5dab4b
hjkim4518 <hjkim4518@cslee.co.kr>
2020-01-16 10:33:34 +0900

    초기화 문제

--- 문제별/01-16 목 5653_swea_줄기세포배양_simulation/김혜진(하는중).cpp
+++ 문제별/01-16 목 5653_swea_줄기세포배양_simulation/김혜진(하는중).cpp
@@ -1,3 +1,5 @@
+// Input을 잘 못 받아오고 있음
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -20,13 +22,22 @@ vector<Cell> addedCell;
 
 void Init() {
 	cellList.clear();
-	for (int i = 0; i < R; i++) {
-		memset(map[i], 0, sizeof(int) * C);
+	addedCell.clear();
+	
+	for (int i = 0; i < bojeong; i++) {
+		memset(map[i], 0, sizeof(int) * bojeong);
+	}
+}
+
+void InitNewMap() {
+	for (int j = 0; j < bojeong; j++) {
+		memset(newMap[j], 0, sizeof(int) * bojeong);
 	}
 }
 
 void Input() {
 	Init();
+	InitNewMap();
 
 	scanf("%d %d %d\n", &R, &C, &T);
 	for (int i = 0; i < R; i++) {
@@ -43,24 +54,17 @@ void Input() {
 }
 
 void Print() {
-	/*for (int i = 0; i < R; i++) {
+	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
-			printf("%d ", map[i][j]);
+			printf("%d ", map[i+500][j+500]);
 		}
 		printf("\n");
-	}*/
+	}
 
 	for (int i = 0; i < cellList.size(); i++) {
 		Cell thisCell = cellList[i];
 		printf("줄기세포 x:%d y:%d 생명력:%d 현재생명력:%d\n", thisCell.r - bojeong, thisCell.c - bojeong, thisCell.hpFix, thisCell.hpNow);
 	}
-}
-int rstd, cstd;
-bool compare(Cell a, Cell b) {
-	if (a.r == b.r) {
-		return a.c <= b.c;
-	}
-	return a.r <= b.r;
 }
 
 int dir[4][2] = { {0, 1}, {0, -1}, {1, 0}, {-1, 0} };
@@ -70,35 +74,27 @@ void Grow(int r, int c) {
 		rtmp = r + dir[i][0];
 		ctmp = c + dir[i][1];
 
-		if (map[rtmp][ctmp] == 0) {
-			if (newMap[rtmp][ctmp] == 0) {
-				addedCell.push_back(Cell{ rtmp, ctmp, map[r][c], 0 });
-				map[rtmp][ctmp] = map[r][c];
-				newMap[rtmp][ctmp] = map[r][c];
-			}
-			else if (newMap[rtmp][ctmp] < map[r][c]) {
-				// 기존 애를 새로운 애로 대체하는 코드
-				// 벡터리스트 수정
-				for (int s = 0; s < cellList.size(); s++) {
-					if (cellList[s].r == rtmp && cellList[s].c == ctmp) {
-						cellList[s].hpFix = map[r][c];
-						cellList[s].hpNow = 0;
-						break;
-					}
-				}
-				// map 수정
-				map[rtmp][ctmp] = map[r][c];
-			}
-			
-		}
-		/*else if (map[rtmp][ctmp] < map[r][c]) {
-			rstd = rtmp;
-			cstd = ctmp;
-			sort(cellList.begin(), cellList.end(), compare); cellList 기준으로 반복문 돌고 있는데 순서 바꾸는 것은 부적절하다
-			cellList[0].hpFix = map[r][c];
-			cellList[0].hpNow = 0;
+		if (map[rtmp][ctmp] == -1 || map[rtmp][ctmp] != 0) continue; // 죽은 애가 있는 자리는 pass
+
+		if (newMap[rtmp][ctmp] == 0) {
+			addedCell.push_back(Cell{ rtmp, ctmp, map[r][c], 0 });
 			map[rtmp][ctmp] = map[r][c];
-		}*/
+			newMap[rtmp][ctmp] = map[r][c];
+		}
+		else if (newMap[rtmp][ctmp] < map[r][c]) { // 죽은 애인지를 검증해야 한다.
+			// 기존 애를 새로운 애로 대체하는 코드
+			// 벡터리스트 수정
+			for (int s = 0; s < cellList.size(); s++) {
+				if (cellList[s].r == rtmp && cellList[s].c == ctmp) {
+					cellList[s].hpFix = map[r][c];
+					cellList[s].hpNow = 0;
+					break;
+				}
+			}
+			// map 수정
+			map[rtmp][ctmp] = map[r][c];
+			newMap[rtmp][ctmp] = map[r][c];
+		}
 	}
 }
 
@@ -108,43 +104,46 @@ int main() {
 	for (int t = 0; t < test_case; t++) {
 		//printf("%d ======================\n", t);
 		Input();
+		InitNewMap();
 		//Print();
 
-		for (int i = 0; i < T; i++) {
+		for (int i = 1; i <= T; i++) {
 			vector<int> deleteCell;
-			
+
+			InitNewMap();
+
 			for (int j = 0; j < cellList.size(); j++) {
 				Cell thisCell = cellList[j];
+				
 				if (thisCell.hpFix > thisCell.hpNow) {
 					cellList[j].hpNow++;
 				}
 				else if (thisCell.hpFix == thisCell.hpNow) {
 					Grow(thisCell.r, thisCell.c);
 					cellList[j].hpNow++;
-					if (cellList[j].hpFix*2 <= cellList[j].hpNow) {
-						deleteCell.push_back(j);
-					}
 				}
-				else if (cellList[j].hpFix*2 <= cellList[j].hpNow) {
+				else {
+					cellList[j].hpNow++;
+				}
+
+				if (cellList[j].hpFix * 2 <= cellList[j].hpNow) { // 죽는지 검사
 					deleteCell.push_back(j);
 				}
-				
+
 			}
+			// 죽이기
 			sort(deleteCell.begin(), deleteCell.end(), greater<int>());
 			int delSize = deleteCell.size();
 			for (int j = 0; j < delSize; j++) {
-				cellList.erase(cellList.begin() + deleteCell[j]);
+				int idx = deleteCell[j];
+				map[cellList[idx].r][cellList[idx].c] = -1; // 죽은 애는 -1로 표현
+				cellList.erase(cellList.begin() + idx);
 			}
 
 			for (int j = 0; j < addedCell.size(); j++) {
 				cellList.push_back(addedCell[j]);
 			}
 			addedCell.clear();
-
-			for (int j = 0; j < R; j++) {
-				memset(newMap[j], 0, sizeof(int) * C);
-			}
-			
 		}
 
 		cout << cellList.size() << endl;
