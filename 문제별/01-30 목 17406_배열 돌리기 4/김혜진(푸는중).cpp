e73d0520a6ceb191c54b1d6cd5b3a7a3b1204842
hjkim4518 <hjkim4518@cslee.co.kr>
2020-01-29 12:56:25 +0900

    배열 원상복구 필요

--- 문제별/01-30 목 17406_배열 돌리기 4/김혜진(푸는중).cpp
+++ 문제별/01-30 목 17406_배열 돌리기 4/김혜진(푸는중).cpp
@@ -20,16 +20,27 @@ void Print();
 void PrintInfos();
 void Turn(int startX, int startY, int endX, int endY, int n);
 int GetMin();
+void Init();
 
+#define MAX 10
+bool Select[MAX];
+vector<int> V;
+void DFS(int cnt);
+
+int MIN = 6000;
 int main() {
 
 	Input();
 
-	for (int i = 0; i < Infos.size(); i++) {
+	DFS(0);
+
+	/*for (int i = 0; i < Infos.size(); i++) {
 		Turn(Infos[i].xStart, Infos[i].yStart, Infos[i].xEnd, Infos[i].yEnd, Infos[i].n);
 		Print();
 		printf("최솟값: %d\n", GetMin());
-	}
+	}*/
+	cout << MIN;
+
 
 	return 0;
 }
@@ -48,7 +59,7 @@ void Input() {
 
 	for (int i = 0; i < K; i++) {
 		scanf("%d %d %d\n", &r, &c, &k);
-		Infos.push_back(Info{r-k, c-k, r+k, c+k, k});
+		Infos.push_back(Info{ r - k, c - k, r + k, c + k, k });
 	}
 }
 
@@ -119,4 +130,89 @@ int GetMin() {
 		sum = 0;
 	}
 	return min;
+}
+
+/*void Init() {
+	for (int i = 1; i < R; i++) {
+		for (int j = 1; j < C; j++) {
+			map[i][j] = backup[i][j];
+		}
+	}
+}*/
+
+void PrintPermutation() {
+	for (int i = 0; i < V.size(); i++) {
+		Info thisInfo = Infos[V[i] - 1];
+		cout << thisInfo.n << " ";
+		
+	}
+	cout << endl;
+}
+
+int copiedMap[60][60];
+
+/*void CopyMap() {
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			copiedMap[i][j] = map[i][j];
+		}
+	}
+}
+
+void RevertMap() {
+	for (int i = 1; i <= R; i++) {
+		for (int j = 1; j <= C; j++) {
+			map[i][j] = copiedMap[i][j];
+		}
+	}
+}*/
+
+int(*BackupMap(int(*map)[60]))[60]{
+	return map;
+}
+
+void DFS(int cnt) {
+	cout << "cnt: " << cnt << endl;
+	if (cnt >= K) {
+		// PrintPermutation();
+		int tmp = GetMin();
+		if (MIN > tmp) {
+			MIN = tmp;
+		}
+
+		return;
+	}
+
+	for (int i = 1; i <= K; i++) {
+		if (Select[i]) continue;
+
+		// CopyMap();
+		int(*origMap)[60] = BackupMap(map);
+		cout << "백업한 오리지널 맵: " << endl;
+		for (int i = 1; i <= C; i++) {
+			cout << origMap[i];
+			cout << endl;
+		}
+
+		Select[i] = true;
+		// V.push_back(i);
+		Info thisInfo = Infos[i-1];
+		Turn(thisInfo.xStart, thisInfo.yStart, thisInfo.xEnd, thisInfo.yEnd, thisInfo.n);
+		cout << cnt+1 << "번째 숫자로 " << i << "선택" << endl;
+		cout << "연산: " << thisInfo.xStart + thisInfo.n << " " << thisInfo.yStart + thisInfo.n << endl;
+		Print();
+		cout << endl;
+
+		DFS(cnt + 1);
+
+		// RevertMap();
+		for (int i = 1; i <= R; i++) {
+			for (int j = 1; j <= C; j++) {
+				map[i][j] = origMap[i][j];
+			}
+		}
+		// V.pop_back();
+
+		Select[i] = false;
+	}
 }
