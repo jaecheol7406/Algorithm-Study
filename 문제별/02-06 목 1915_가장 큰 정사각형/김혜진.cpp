eaab3eb9e2269a0071d3457eb05d939e3b9359f0
Karolline <hj264@hanmail.net>
2020-02-05 21:20:16 +0900

    solve

--- 문제별/02-06 목 1915_가장 큰 정사각형/김혜진.cpp
+++ 문제별/02-06 목 1915_가장 큰 정사각형/김혜진.cpp
@@ -6,11 +6,12 @@ using namespace std;
 struct Pos {
 	int r;
 	int c;
+	int max;
 };
 
 int R, C;
 int map[1005][1005];
-int maxSize[1005][1005];
+int sumMap[1005][1005];
 vector<Pos> cands;
 
 int MAX = 0;
@@ -18,29 +19,26 @@ int MAX = 0;
 void Input() {
 	scanf("%d %d\n", &R, &C);
 	for (int i = 0; i < R; i++) {
+		int sum = 0;
 		for (int j = 0; j < C; j++) {
 			scanf("%1d", &map[i][j]);
 			if (map[i][j] == 1) {
-				maxSize[i][j] = min(R - i, C - j);
-				cands.push_back(Pos{ i, j });
+
+				cands.push_back(Pos{ i, j, min(R - i, C - j) });
 				MAX = 1;
 			}
-			
+
+			sum += map[i][j];
+			sumMap[i][j] = sum;
 
 		}
 	}
 }
 
 void Print() {
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			printf("%d ", maxSize[i][j]);
-		}
-		cout << endl;
-	}
 
 	for (int i = 0; i < cands.size(); i++) {
-		cout << cands[i].r << ", " << cands[i].c << endl;
+		cout << cands[i].r << ", " << cands[i].c << " , " << cands[i].max << endl;
 	}
 }
 
@@ -52,45 +50,65 @@ bool CanDo(int thisR, int thisC, int size) {
 	if (R <= stdR || C <= stdC) return false;
 
 	for (int i = thisR; i <= stdR; i++) {
-		for (int j = thisC; j <= stdC; j++) {
-			if (map[i][j] != 1) {
-				return false;
-			}
-		}
+		if (sumMap[i][stdC] - sumMap[i][thisC-1] != (size)) return false;
 	}
+
+	/*for (int i = thisR; i <= stdR; i++) {
+		for (int j = thisC; j <= stdC; j++) {
+			if (map[i][j] == 0) return false;
+		}
+	}*/
 
 	return true;
 }
 
-void DFS(int n) {
+bool Check(int n) {
 	int thisR = cands[n].r;
 	int thisC = cands[n].c;
-	// cout << "이번좌표: " << thisR << "," << thisC << endl;
+	int thisMax = cands[n].max;
+	// cout << "이번좌표: " << thisR << "," << thisC << " max: " << thisMax << endl;
 
-	for (int size = maxSize[thisR][thisC]; size > MAX; size--) {
+	for (int size = thisMax; size > MAX; size--) {
 		// 정사각형을 만들 수 있는지 확인
 
 		if (CanDo(thisR, thisC, size)) {
+			// cout << "현재 MAX: " << MAX << "이번 정사각형: " << size << "짜리" << endl;
 			if (MAX < size) {
 				MAX = size;
 			}
+			return true; // 어차피 size는 점점 작아지므로 먼저 발견됐으면 뒤 비교 할 필요 없음
 		}
 
-		
+
 	}
+	return false;
+}
+
+bool cmp(Pos posA, Pos posB) {
+	if (posA.max == posB.max && posA.r == posB.r) {
+		return posA.c < posB.c;
+	}
+	if (posA.max == posB.max) {
+		return posA.r < posB.r;
+	}
+	return posA.max > posB.max;
 }
 
 int main() {
 
 	Input();
+
+	// max 기준으로 내림차순 정렬
+	sort(cands.begin(), cands.end(), cmp);
+
 	// Print();
 
 	for (int i = 0; i < cands.size(); i++) {
-		if (maxSize[cands[i].r][cands[i].c] <= MAX) continue;
-		DFS(i);
+		if(cands[i].max > MAX)
+			Check(i);
 	}
 
-	cout << MAX*MAX;
+	cout << MAX * MAX;
 
 	return 0;
 }
