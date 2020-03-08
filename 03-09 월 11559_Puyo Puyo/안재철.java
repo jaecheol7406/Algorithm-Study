b46d1dd0dba727437775c73ada0c114f7c7370cd
안재철 <qnzo8888@gmail.com>
2020-03-09 07:13:10 +0900

    Update 안재철.java

--- 03-09 월 11559_Puyo Puyo/안재철.java
+++ 03-09 월 11559_Puyo Puyo/안재철.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 /*
  * 11559_Puyo Puyo
  * 1. char형 2차원배열 field에 입력
- * 2. bfs로 field 탐색해서 부서질 뿌요의 모든 좌표들 deletePos에 저장
- * 		deletePos는 key : 열좌표값, value : 행좌표값들을 저장하는 HashMap임.  즉 부서질 뿌요의 좌표값들을 열 좌표값 별로 저장.
+ * 2. bfs로 field 탐색해서 부서질 뿌요의 모든 좌표들을 '.'로 채움
  * 3. 부서질 뿌요가 없으면 break 후 combo 횟수 출력 
- * 4. 있으면 deletePos 순회하여 열별로 붙어있는 행 개수만큼 밑으로 내린다. (down() 함수)
+ * 4. 있으면 '.' 제외한 문자들을 내리고 나머지는 '.'로 채움 (down() 함수)
  * 5. combo++
  * 6. 2번으로 돌아가서 반복
  */
@@ -22,7 +19,6 @@ public class Main {
 	static char[][] field = new char[12][6];
 	static boolean[][] visit = new boolean[12][6];
 	
-	static HashMap<Integer, ArrayList<Integer>> deletePos = new HashMap<>();
 	static int combo;
 	
 	public static void main(String[] args) throws Exception {
@@ -39,28 +35,22 @@ public class Main {
 		int startX = 0, startY = 0;
 		int nextStartX = 0, nextStartY = 0;
 		while(true) {
-			boolean meet = false;
+			boolean pop = false;
 			for(int i = startX; i < 12; i++) {
 				for(int j = startY; j < 6; j++) {
 					if(!visit[i][j] && field[i][j] != '.') {
-						if(!meet) {
-							meet = true;
-							nextStartX = i;
-							nextStartY = j;
-						}
-						
-						bfs(i, j);
+						if(bfs(i, j))
+							pop = true;
 					}
 				}
 			}
 			
-			if(deletePos.isEmpty())
+			if(!pop)
 				break;
 			
 			down();
 			combo++;
 			
-			deletePos = new HashMap<>();
 			visit = new boolean[12][6];
 			
 			startX = nextStartX;
@@ -69,7 +59,7 @@ public class Main {
 		System.out.println(combo);
 	}
 	
-	static void bfs(int x, int y) {
+	static boolean bfs(int x, int y) {
 		Queue<int[]> q = new LinkedList<>();
 		q.add(new int[]{x, y});
 		char color = field[x][y];
@@ -96,47 +86,29 @@ public class Main {
 		}
 		
 		if(poss.size() >= 4) {
-			for(int[] pos : poss) {
-				ArrayList<Integer> xs = deletePos.get(pos[1]);
-				if(xs == null)
-					xs = new ArrayList<>();
-				xs.add(pos[0]);
-				deletePos.put(pos[1], xs);
-			}
+			for(int[] pos : poss)
+				field[pos[0]][pos[1]] = '.';
+			return true;
 		}
+		return false;
 	}
 	
 	static void down() {
 		// 한 줄(열)씩 수행
-		for(int y : deletePos.keySet()) {
-			ArrayList<Integer> xs = deletePos.get(y);
-			Collections.sort(xs);
-			
-			int firstIdx = xs.get(0);
-			int prev = firstIdx;
-			for(int i = 1; i < xs.size(); i++) {
-				int now = xs.get(i);
-				if(now - prev == 1) {
-					prev++;
-					continue;
-				}
-				
-				int popLen = prev - firstIdx + 1;
-				// down
-				for(int movedIdx = firstIdx - 1; movedIdx >= 0; movedIdx--)
-					field[movedIdx + popLen][y] = field[movedIdx][y];
-				for(int j = 0; j < popLen; j++)
-					field[j][y] = '.';
-				
-				firstIdx = now;
-				prev = now;
+		for(int j = 0; j < 6; j++) {
+			ArrayList<Character> temp = new ArrayList<>();
+			for(int i = 0; i < 12; i++) {
+				if(field[i][j] != '.')
+					temp.add(field[i][j]);
 			}
-			int popLen = prev - firstIdx + 1;
-			// down
-			for(int movedIdx = firstIdx - 1; movedIdx >= 0; movedIdx--)
-				field[movedIdx + popLen][y] = field[movedIdx][y];
-			for(int j = 0; j < popLen; j++)
-				field[j][y] = '.';
+			if(temp.size() == 0)
+				continue;
+			
+			int blank = 12 - temp.size();
+			for(int i = 0; i < blank; i++)
+				field[i][j] = '.';
+			for(int i = blank; i < 12; i++)
+				field[i][j] = temp.remove(0);
 		}
 	}
 }
