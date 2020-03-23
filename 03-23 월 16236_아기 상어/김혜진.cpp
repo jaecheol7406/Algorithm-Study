bbb3d66d4f527a5734fa60ea90f2acdd052b47b8
Karolline <hj264@hanmail.net>
2020-03-23 22:09:52 +0900

    주석 추가

--- 03-23 월 16236_아기 상어/김혜진.cpp
+++ 03-23 월 16236_아기 상어/김혜진.cpp
@@ -1,4 +1,10 @@
 /*
+함수 호출 순서: Input() -> BFS()
+
+1. BFS로 "먹을 수 있고", "가장 가까우면서", "가장 위&왼쪽에 있는 물고기"를 골라낸다.
+2. 그 물고기가 있는 곳까지 가서, 먹고, 크기를 키울 수 있으면 키운다.
+3. 2에서 업데이트된 조건을 반영해 "먹을 수 있는 물고기가 없을 때까지" 1을 계속한다.
+
 주의점 1: 특정 위치와의 거리를 단순 abs로 계산하면 안 된다.(나보다 큰 상어는 지나갈 수 없기 때문에)
 주의점 2: n개 거리가 떨어진 BFS를 돌 때, 무조건 가장 먼저 도달한 곳으로 가면 안 된다.(같은 거리의 BFS일 때 큐에서 더 뒤에 위치한 애가 '더 위, 더 왼쪽' 일 수 있다
 */
@@ -14,21 +20,33 @@ struct Pos {
 	int c;
 };
 
-
+/* 기본 Input */
 int N;
 Pos shark; // 상어의 위치
 int eaten = 0; // 상어가 먹은 물고기 개수
 int sharkSize = 2; // 상어 크기
-
-int stdDist = 0;
-
 int fish[25][25]; // 물고기의 위치 및 물고기 크기
-int visited[25][25];
-int distArr[25][25];
 
-int seconds;
 
+
+/* BFS 관련 변수 */
+queue<Pos> q; // BFS에서 먹을 수 있는 물고기를 발견한 가장 작은 cnt
+int dir[4][2] = { {-1, 0}, {0, -1}, {0, 1}, {1, 0} }; // 이 순서는 아무것도 보장하지 않음
+
+int visited[25][25]; // BFS의 visited
+int distArr[25][25]; // BFS에서 몇 걸음 간 것인지 cnt 기록
+int stdDist = 0; // BFS에서 stdDist 걸음만큼 간 위치들 중, 먹을 수 있는 물고기 목록 (이 중 가장 위, 가장 왼쪽인 물고기를 판별해야 함)
 vector<Pos> cands;
+
+
+
+// cands를 정렬하는 데 사용하는 함수
+bool cpr(Pos a, Pos b) {
+	if (a.r == b.r) return a.c < b.c;
+	return a.r < b.r;
+}
+
+int seconds; // 총 걸린 시간 (물고기까지의 거리의 총합산)
 
 void Input() {
 	scanf("%d\n", &N);
@@ -43,23 +61,7 @@ void Input() {
 	}
 }
 
-void Print() {
-	cout << shark.r << "," << shark.c << endl;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			printf("%d ", fish[i][j]);
-		}
-		cout << endl;
-	}
-}
-queue<Pos> q;
 
-bool cpr(Pos a, Pos b) {
-	if (a.r == b.r) return a.c < b.c;
-	return a.r < b.r;
-}
-
-int dir[4][2] = { {-1, 0}, {0, -1}, {0, 1}, {1, 0} };
 Pos BFS() {
 	// Init
 	for (int i = 0; i < N; i++) {
@@ -78,15 +80,13 @@ Pos BFS() {
 	stdDist = 0;
 
 	q.push(shark);
-	// visited[shark.r][shark.c] = 1;
-	// cout << "현재 상어 위치: " << shark.r << "," << shark.c << endl;
 	int thisR, thisC;
 	int tmpR, tmpC;
 
 	while (!q.empty()) {
 
 		thisR = q.front().r; thisC = q.front().c;
-		// cout << "이번 큐: " << thisR << "," << thisC << endl;
+
 		if (fish[thisR][thisC] > 0 && fish[thisR][thisC] < sharkSize) { // 먹을 수 있는 가장 가까운 물고기
 			return Pos{ thisR, thisC };
 		}
@@ -100,32 +100,25 @@ Pos BFS() {
 
 			if (tmpR < 0 || tmpC < 0 || N <= tmpR || N <= tmpC) continue;
 			if (visited[tmpR][tmpC] != 0) continue;
-			// cout << tmpR << "," << tmpC << "(사이즈" << fish[tmpR][tmpC] << ") <? " << sharkSize << endl;
+
 			if (fish[tmpR][tmpC] > sharkSize) continue;
 			
-			if (fish[tmpR][tmpC] > 0 && fish[tmpR][tmpC] < sharkSize) { // 먹을 수 있는 가장 가까운 물고기
+			if (fish[tmpR][tmpC] > 0 && fish[tmpR][tmpC] < sharkSize) { // 먹을 수 있는 물고기
 				distArr[tmpR][tmpC] = distArr[thisR][thisC] + 1;
 				stdDist = distArr[tmpR][tmpC];
-				// cout << "!!" << endl;
 				cands.push_back(Pos{ tmpR, tmpC });
-				// return Pos{ tmpR, tmpC };
+				// return Pos{ tmpR, tmpC }; 주의점 2 와 관련된 틀리게 하는 원인
 			}
 			else {
 				visited[tmpR][tmpC] = 1;
 				q.push(Pos{ tmpR, tmpC });
 				distArr[tmpR][tmpC] = distArr[thisR][thisC] + 1;
-				// cout << "distArr[tmpR][tmpC]= " << distArr[thisR][thisC] + 1 << endl;
 			}
-			
 			
 		}
 	}
 
-	// cout << "cands size: " << cands.size() << endl;
 	if (cands.size() > 0) {
-		/*for (int i = 0; i < cands.size(); i++) {
-			cout << cands[i].r << "," << cands[i].c << "(" << distArr[cands[i].r][cands[i].c] << ")" << endl;
-		}*/
 		sort(cands.begin(), cands.end(), cpr);
 
 		
@@ -134,24 +127,22 @@ Pos BFS() {
 	else {
 		return Pos{ -1, -1 };
 	}
-	
 }
+
 
 int main() {
 
 	Input();
 
 	Pos nextPos = Pos{ 0, 0 };
-	// 먹을 수 있는 물고기를 찾는다.(BFS)
-	while (1) {
-		
+	
+	while (1) { // 먹을 수 있는 물고기를 찾는다.(BFS)
 
 		nextPos = BFS();
 		if (nextPos.r == -1 && nextPos.c == -1) {
 			break;
 		}
 
-		// cout << "****먹는다: " << nextPos.r << "," << nextPos.c << " 이동거리: " << distArr[nextPos.r][nextPos.c] << endl;
 		seconds += distArr[nextPos.r][nextPos.c];
 		cands.clear();
 
@@ -164,7 +155,6 @@ int main() {
 			sharkSize++;
 		}
 		
-		// cout << "크기: " << sharkSize << " 먹은 개수: " << eaten << endl;
 	}
 	
 	cout << seconds;
