839160cb764d3dd17ccf05eaa1f22eca1b505b2b
karlema <30691193+karlema@users.noreply.github.com>
2020-03-25 20:48:33 +0900

    Create 김주성(성공).cpp

---
+++ 03-23 월 15662_톱니바퀴 (2)/김주성(성공).cpp
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <deque>
+using namespace std;
+int t;
+int k;
+
+int copy2[1001][8];
+int result;
+int chk[1001][8];
+void calcul(vector<deque<int>>& v)
+{
+	for (int i = 0; i < t; i++)
+	{
+		if (v[i][0] == 1)
+			result++;
+	}
+}
+void copy(vector<deque<int>>& v)
+{
+	for (int i = 0; i < t; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			copy2[i][j] = v[i][j];
+		}
+		//printf("\n");
+	}
+}
+
+void print(vector<deque<int>>& v)
+{
+	for (int i = 0; i < t; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			printf("%d", v[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+void rotate(int dir, int idx, vector<deque<int>>& v)
+{
+	//printf("d : %d i : %d\n",dir,idx);
+	if (dir == 1)
+	{
+		int temp = 0;
+		temp = v[idx].back();
+		v[idx].pop_back();
+		v[idx].push_front(temp);
+	}
+	else
+	{
+		int temp2 = 0;
+		temp2 = v[idx].front();
+		v[idx].pop_front();
+		v[idx].push_back(temp2);
+	}
+	//print(v);
+}
+void check(int num, int dir, vector<deque<int>>& v)
+{
+	// dir이 1이면 시계방향 -1이면 반시계 방향
+	// print(v);
+	//printf("\nstart : %d %d\n", num, dir);
+	// 오른쪽을 본다.
+	int temp2 = dir * -1;
+	for (int i = num+1; i < t; i++)
+	{
+		// 만약 다른 극이라면
+		if (copy2[i][6] != copy2[i - 1][2])
+		{
+			//printf("오른쪽\n");
+			
+			rotate(temp2, i, v);
+			temp2 = temp2 * -1;
+		}
+		else // 만약 같은 극이라면
+		{
+			break;
+		}
+	}
+	// 왼쪽을 본다.
+	temp2 = dir * -1;
+	for (int i = num-1; i >= 0; i--)
+	{
+		// 만약 다른 극이라면
+		if (copy2[i][2] != copy2[i + 1][6])
+		{
+			//printf("왼쪽\n");
+			rotate(temp2, i, v);
+			temp2 = temp2 * -1;
+		}
+		else // 만약 같은 극이라면
+		{
+			break;
+		}
+	}
+	rotate(dir, num, v);
+}
+int main()
+{
+	scanf("%d", &t);
+	vector<deque<int>>v(t);
+	int a = 0;
+	for (int i = 0; i < t; i++)
+	{
+		for (int j = 1; j <= 8; j++)
+		{
+			scanf("%1d", &a);
+			v[i].push_back(a);
+		}
+	}
+	scanf("%d", &k);
+	int num = 0, dir = 0;
+	for (int i = 0; i < k; i++)
+	{
+		//print(v);
+		// 벡터를 복사한다.
+		copy(v);
+		scanf("%d %d", &num, &dir);
+		check(num - 1, dir, v);
+		//recopy(v,copy2);
+		//print(v);
+		// 복사한 벡터에 다시 넣는다.
+	}
+	calcul(v);
+	printf("%d", result);
+	return 0;
+}
