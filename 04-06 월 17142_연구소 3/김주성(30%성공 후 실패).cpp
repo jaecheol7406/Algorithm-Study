309fcbfcd5f562443542cc67ea068638d437936c
karlema <30691193+karlema@users.noreply.github.com>
2020-04-09 09:56:39 +0900

    Update and rename 김주성(실패).cpp to 김주성(30%성공 후 실패).cpp

--- 04-06 월 17142_연구소 3/김주성(30%성공 후 실패).cpp
+++ 04-06 월 17142_연구소 3/김주성(30%성공 후 실패).cpp
@@ -1,3 +1,5 @@
+// 30%에서 틀렸습니다.
+// init()함수로 초기화를 먼저하고 dfs()함수로 조합을 생성후 bfs()함수로 퍼트려가면서 구했다.
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <queue>
@@ -16,6 +18,8 @@ queue <pair<int, int>>q;
 int dy[4] = { 0,0,1,-1 };
 int dx[4] = { 1,-1,0,0 };
 int cnt;
+int total;
+int virus;
 void init()
 {
 	for (int i = 0; i < n; i++)
@@ -24,6 +28,7 @@ void init()
 		{
 			tempmap[i][j] = map[i][j];
 			visit[i][j] = 0;
+			virus = 0;
 		}
 	}
 }
@@ -59,30 +64,58 @@ void vectprint()
 	}
 	printf("\n");
 }
+void printvisit()
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d ", visit[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
 void bfs()
 {
 	//printf("\nstart : ");
+	cnt = 0;
+	
 	while (!q.empty())
 	{
+		
 		int qsize = q.size();
 		int cy = q.front().first;
 		int cx = q.front().second;
+		//printf("%d %d\n",cy,cx);
 		visit[cy][cx] = 1;
+		//printf("bfs start %d %d\n", virus, total);
+		
 		//printf("%d\n",cnt);
 		q.pop();
 		for (int i = 0; i < 4; i++)
 		{
 			int ny = cy + dy[i];
 			int nx = cx + dx[i];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= n || visit[ny][nx] == 1 || tempmap[ny][nx] == 45 || tempmap[ny][nx] == 42) continue;
+			if (ny < 0 || nx < 0 || ny >= n || nx >= n || visit[ny][nx] == 1 || tempmap[ny][nx] == 45) continue;
 			q.push(make_pair(ny, nx));
+			//printf("%d %d\n", virus, total);
+			if (tempmap[ny][nx] == 0) virus++;
 			visit[ny][nx] = 1;
-			tempmap[ny][nx] = tempmap[cy][cx]+1;
+			tempmap[ny][nx] = tempmap[cy][cx] + 1;
 			cnt = tempmap[ny][nx];
 		}
-	}
-	if (result > cnt) result = cnt;
 
+		//printf("%d", cnt);
+		if (virus == total)
+		{
+			if (result > cnt) result = cnt;
+			//printf("결과 : %d\n", result);
+			break;
+		}
+	}
+	
+	
 	return;
 }
 void dfs(int cnt, int y, int x)
@@ -100,8 +133,9 @@ void dfs(int cnt, int y, int x)
 				if (back[i][j] == 1)
 				{
 					//printf("bfs start %d %d\n", i, j);
-					tempmap[i][j] = 0;
 					q.push(make_pair(i, j));
+					tempmap[i][j] = 0;
+					
 				}
 				if (tempmap[i][j] == 2)	tempmap[i][j] = '*';
 				if (tempmap[i][j] == 1) tempmap[i][j] = '-';
@@ -109,10 +143,11 @@ void dfs(int cnt, int y, int x)
 		}
 		//print();
 		bfs();
-		printf("\n");
+		//printf("\n");
 		//backprint();
-		print();
-		printf("%d\n",result);
+		//print();
+		//printvisit();
+		//printf("%d\n",result);
 		init();
 		return;
 	}
@@ -127,7 +162,6 @@ void dfs(int cnt, int y, int x)
 				dfs(cnt + 1, i, j); //인자를 조심해서 넣어라 필자는 y,x를 넣어서 시간을 많이 잡아먹음.
 				back[i][j] = 0;
 			}
-			
 		}
 		x = 0;
 	}
@@ -149,11 +183,22 @@ int main()
 		for (int j = 0; j < n; j++)
 		{
 			scanf("%d", &map[i][j]);
-			if (map[i][j] == 2) v.push_back(make_pair(i, j));
+			// 벽은 -1 , 바이러스 -2 빈곳은 -3으로표시
+			if (map[i][j] == 2)
+			{
+				v.push_back(make_pair(i, j));
+			}
+			if (map[i][j] == 0)	total++;
 		}
+	};
+	if (0 == total)
+	{
+		printf("0");
+		return 0;
 	}
 	init();
 	dfs(0, 0, 0);
+	if (result == 987654321) result = -1;
 	printf("%d", result);
 	return 0;
 }
