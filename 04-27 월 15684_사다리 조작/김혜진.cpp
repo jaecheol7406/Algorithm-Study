03aa6b54712fd2317c81c8e4a246d311e7ce7796
Karolline <hj264@hanmail.net>
2020-04-29 11:13:00 +0900

    solved

--- 04-27 월 15684_사다리 조작/김혜진.cpp
+++ 04-27 월 15684_사다리 조작/김혜진.cpp
@@ -1,8 +1,9 @@
 /*
-틀린코드
-사다리 1개로 되나 확인해보고, 안되면 2개, 또 안되면 3개를 해야되는데
-아래 코드는 (1, 1)에 사다리를 설치하고 이게 답이 아니면 그 상태에서 하나를 추가해 2개를 설치
-그 다음에 답을 체크해서 맞게되면 리턴하는데, 만약 더 아래에 사다리 1개짜리로 만들수있는것이 있을수도있는데 그건 탐색이 되지않은채로 2를 리턴하기 때문에 오답
+헷갈리는 부분
+1. DFS 탐색완료시 조기종료시키기 (전역변수 answer 활용)
+2. 사다리 위치를 고를 때, 순열이 아니라 조합으로 뽑아야 함.
+   1,3 순회 때 (1,3) & (3, 3) 을 뽑아놓고, 
+   3, 3 순회에서 또 (3, 3) & (1,3)을 뽑으면 시간초과
 */
 #include <iostream>
 #include <vector>
@@ -11,11 +12,14 @@ using namespace std;
 int N, M, H;
 int map[35][15];
 
+int limit;
+int answer = -1;
+
 void Input() {
-	scanf("%d %d %d", &N, &M, &H);
+	cin >> N >> M >> H;
 	int a, b;
 	for (int i = 0; i < M; i++) {
-		scanf("%d %d\n", &a, &b);
+		cin >> a >> b;
 		map[a][b] = 1;
 	}
 }
@@ -45,69 +49,48 @@ bool Test() {
 	return true;
 }
 
+void DFS(int pre, int cnt) {
+	if (answer != -1) return; // 주의점 1. 답이 이미 나왔음, DFS를 중간에 종료시키는 방법
+
+	if (cnt >= limit) {
+
+		if (Test()) {
+			answer = limit;
+		}
+		//cout << endl;
+		//Print();
+		return;
+	}
+
+	for (int i = 1; i <= H; i++) {
+		for (int j = pre; j < N; j++) {
+			if (map[i][j] ) continue;
+			if (j - 1 >= 1 && map[i][j - 1] == 1) continue;
+			if (j + 1 < N && map[i][j + 1] == 1) continue;
+
+			map[i][j] = 1;
+			//cout << cnt << "번째 선택: " << "(" << i << "," << j << ") ";
+			DFS(pre + 1, cnt + 1); // 2. 사다리 위치를 고를 때, 순열이 아니라 조합으로 뽑아야 함.
+			if (answer != -1) break;  // 주의점 1. 답이 이미 나왔음, DFS를 중간에 종료시키는 방법
+			map[i][j] = 0;
+		}
+	}
+
+}
+
 int main() {
 
 	Input();
 	// Print();
 
-	if (Test()) {
-		cout << 0;
-		return 0;
+	for (int i = 0; i < 4; i++) {
+		limit = i;
+
+		DFS(1, 0); // 2. 사다리 위치를 고를 때, 순열이 아니라 조합으로 뽑아야 함.
+		if (answer != -1) break;  // 주의점 1. 답이 이미 나왔음, DFS를 중간에 종료시키는 방법
 	}
 
-	for (int j1 = 1; j1 < N; j1++) { // 몇 번째 열에 놓을 것인가 (j ~ j+1)
-		for (int i1 = 1; i1 <= H; i1++) { // 몇 번에 행에 놓을 것인가
-			if (map[i1][j1] == 1) continue;
-
-			map[i1][j1] = 1;
-			// cout << "test: (" << i1 << "," << j1 << ")" << endl;
-			// Print();
-
-			if (Test()) {
-				cout << 1;
-				return 0;
-			}
-
-			// 2번째거 골라본다
-			for (int j2 = 1; j2 < N; j2++) {
-				for (int i2 = 1; i2 <= H; i2++) {
-					if (j1 == j2 && i1 == i2) continue;
-					if (map[i2][j2] == 1) continue;
-
-					map[i2][j2] = 1;
-					// cout << "test: (" << i1 << "," << j1 << ") & (" << i2 << "," << j2 << ")" << endl;
-					// Print();
-					if (Test()) {
-						cout << 2;
-						return 0;
-					}
-
-					// 3번째거 골라본다
-					for (int j3 = 1; j3 < N; j3++) {
-						for (int i3 = 1; i3 <= H; i3++) {
-							if (j1 == j3 && i1 == i3) continue;
-							if (j2 == j3 && i2 == i3) continue;
-							if (map[i3][j3] == 1) continue;
-
-							map[i3][j3] = 1;
-							// cout << "test: (" << i1 << "," << j1 << ") & (" << i2 << "," << j2 << ") & (" << i3 << "," << j3 << ")" << endl;
-							// Print();
-							if (Test()) {
-								cout << 3;
-								return 0;
-							}
-							map[i3][j3] = 0;
-						}
-					}
-					map[i2][j2] = 0;
-				}
-			}
-
-			map[i1][j1] = 0;
-		}
-	}
-
-	cout << -1;
+	cout << answer;
 
 	return 0;
 }
