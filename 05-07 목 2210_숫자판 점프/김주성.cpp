3f3a43cf558f5dbe9a0635feaecb33fca1f9d6e1
karlema <30691193+karlema@users.noreply.github.com>
2020-05-06 09:20:09 +0900

    Create 김주성.cpp

---
+++ 05-07 목 2210_숫자판 점프/김주성.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <set>
+using namespace std;
+int map[5][5];
+int dy[4] = { 0,0,1,-1 };
+int dx[4] = {1,-1,0,0};
+set<int>s;
+void dfs(int cur,int y,int x,int sum)
+{
+	if (cur==5)
+	{
+		s.insert(sum);
+		//printf("%d\n", sum);
+		return;
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= 5 || nx >= 5) continue;
+		dfs(cur + 1, ny, nx, sum * 10 + map[ny][nx]);
+	}
+	
+}
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			scanf("%d",&map[i][j]);
+		}
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			dfs(0, i, j, map[i][j]);
+		}
+	}
+	printf("%d",s.size());
+	return 0;
+}
